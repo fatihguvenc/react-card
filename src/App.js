8ca1bd0c81678bdf4504033e880abfb4c678c1f4
fatihgvnc <heatclife007@gmail.com>
2020-12-22 04:18:45 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import DATA_LIST from "./data/data.js";
 
 export default function App() {
   const CardComp = DATA_LIST.map((item, index) => (
-    <Card imgURL={item.imgURL} key={index} />
+    <Card
+      imgURL={item.imgURL}
+      title={item.title}
+      secondTitle={item.secondTitle}
+      price={item.price}
+      key={index}
+    />
   ));
   return (
     <div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="col-sm-3 mt-5">
+    <div className="col-sm-4 mt-5">
       <div className="card">
         <img src={props.imgURL} className="card-img-top" alt="..." />
         <div className="card-body">
-          <p className="card-text">
-            Some quick example text to build on the card title and make
-            up the bulk of the card's content.
-          </p>
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.secondTitle}</p>
+          <h5 className="card-title">{props.price}</h5>
         </div>
       </div>
     </div>
