c1708c4610d8a840044692c95e0027c350a91397
fatihgvnc <heatclife007@gmail.com>
2020-12-22 23:16:34 +0300

    1 file modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 export default function Footer() {
   return (
-    <footer class="text-muted py-5">
-      <div class="container">
-        <p class="float-end mb-1">
+    <footer className="text-muted py-5">
+      <div className="container">
+        <p className="float-end mb-1">
           <a href="#">Back to top</a>
         </p>
-        <p class="mb-1">
+        <p className="mb-1">
           Album example is © Bootstrap, but please download and customize it for
           yourself!
         </p>
-        <p class="mb-0">
+        <p className="mb-0">
           New to Bootstrap? <a href="/">Visit the homepage</a> or read our{" "}
           <a href="/docs/5.0/getting-started/introduction/">
             getting started guide
